c6a9a2e073190b2db575b6ed6d5dc36a178042a5
amine <amineakour6@gmail.com>
2024-08-18 00:39:13 +0100

    update header timer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Headers({score, pause, setPause, gameOver, dificultyLeve
             <h2>Score: {score}</h2>
             <div className="flex gap-5 items-center">
                 <p>Level: {dificultyLevelInStr}</p>
-                <p>Timer: {Math.round(timer / 100)}</p>
+                <p>Timer: {(timer / 100).toFixed(1)}s</p>
                 <button onClick={() => setPause(v => !v)} className="hover:bg-gray-300 w-8 h-8 rounded-full"><i className={`fa-solid ${(pause || gameOver) ? "fa-play" : "fa-pause"}`}></i></button>
                 <button className="btn-1" onClick={e =>  window.location.reload()}>
                     <i className="fa-solid fa-reply"></i>

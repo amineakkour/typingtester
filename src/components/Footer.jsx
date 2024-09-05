7ce73468974e4c91dfee399977b973d5c8cf72bf
amineakkour <amineakour6@gmail.com>
2024-09-05 13:25:41 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,10 @@ export default function Footer() {
             <div className="text-xs text-stone-600 p-2 px-6 flex justify-between">
                 <div className="flex gap-6">
                     <p>Characters must not cross the line above!</p>
-                    <p>You can click Space to pause the game</p>
+                    <span>|</span>
+                    <p>Click Space to pause the game</p>
                 </div>            
-                <div>Developed by Mohamed Amine Akkour</div>
+                <div>All rights reserved &copy; 2023</div>
             </div>
         </div>
     )

d600961e0781b2825040cb6ccad5c6ac3211a2a6
amineakkour <amineakour6@gmail.com>
2024-09-05 17:21:40 +0100

    add device not allowed

--- src/components/Skelaton.jsx
+++ src/components/Skelaton.jsx
@@ -1,15 +1,28 @@
 import Starter from './Starter'
 import Game from './Game';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Skelaton() {
     const [started, setStarted] = useState(false);
     const [dificultyLevel, setDificultyLevel] = useState(parseInt(localStorage.getItem("dificulty_level")) || 0);
     const [allowMistakes, setAllowMistakes] = useState(true);
+    const [deviceAllowed, setDeviceAllowed] = useState(true);
 
-    if (started) {
-        return <Game setStarted={setStarted} dificultyLevel={dificultyLevel} allowMistakes={allowMistakes} />;
-    } else {
-        return <Starter allowMistakes={allowMistakes} setAllowMistakes={setAllowMistakes} setStarted={setStarted} dificultyLevel={dificultyLevel} setDificultyLevel={setDificultyLevel} />
+    useEffect(() => {
+        if(window.outerWidth < 700) setDeviceAllowed(false);
+    }, [])
+
+    if(deviceAllowed) {
+        if (started) {
+            return <Game setStarted={setStarted} dificultyLevel={dificultyLevel} allowMistakes={allowMistakes} />;
+        } else {
+            return <Starter allowMistakes={allowMistakes} setAllowMistakes={setAllowMistakes} setStarted={setStarted} dificultyLevel={dificultyLevel} setDificultyLevel={setDificultyLevel} />
+        }
+    }else{
+        return <div className='p-5'>
+            <h3 className='text-xl text-red-500'>Opps, It looks your device is not allowed</h3>
+            <p>You must to log in using a Desktop</p>
+            <button className='mt-4 btn-1' onClick={() => setDeviceAllowed(true)}>Continue Anyway</button>
+        </div>
     }
 }
